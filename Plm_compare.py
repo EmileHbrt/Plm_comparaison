13e2fb7b112460f963a1a2c08611891462044323
Emile Hembert <emile.hembert@gmail.com>
2025-03-15 14:01:35 +0100

    création du main() et importation des arguments 2nd try

--- Plm_compare.py
+++ Plm_compare.py
@@ -1,4 +1,25 @@
 # Plm_comparaison Dorian & Emile 
 
 from optparse import OptionParser
-print('hi')
+
+#==================================================================================================================
+
+def main():
+	usage = usage = "python Parser_tab.py -i <input_file> -o <output_file> -s <score_arg> \n"
+	parser = OptionParser(usage)
+	parser.add_option("-i", "--input_file", dest="input_file", help="path for the dataset")
+	parser.add_option("-o", "--output_file", dest="output_file", help="path for the file parsed")
+	parser.add_option("-s", "--score_arg", dest="score_arg", help="desired score limit")
+	
+	(options, args) = parser.parse_args()
+	input_file = options.input_file
+	output_file = options.output_file
+	score_arg = options.score_arg
+	print('input :' , input_file)
+	print('output :' , output_file)
+	print('score :' , score_arg)
+	
+#==================================================================================================================
+
+if __name__ == "__main__":
+	main()
