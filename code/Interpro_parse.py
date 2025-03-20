b043c6f5693a2dbbf76b30b3698d31a9bb1fd865
DLRX <dodo.leroux@outlook.com>
2025-03-20 16:07:49 +0100

    interpro_parse.py final version
    parsage d'interpro en ligne de commande OptionParser, fournir le fichier xml d'interpro et un dossier pour l'output des csv créer

--- code/Interpro_parse.py
+++ code/Interpro_parse.py
@@ -1,9 +1,7 @@
 import os
 import csv
-
-import pandas as pd
+from tqdm import tqdm
 import xml.etree.ElementTree as ET
-
 from optparse import OptionParser
 
 #==================================================================================================================
@@ -32,7 +30,17 @@ def writter_ec(dico, output):
 
 #==================================================================================================================
 
-#def writter_map():
+def writter_map_interpro(map_dict, name_dict, output):
+    with open(output +"\interpro_map_id.csv", mode='w', newline='',encoding='utf-8') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow([key for key,val in name_dict.items()])
+        for interpro_id, entries in map_dict.items(): # entries is a list with db and dbkey associated
+            for entry in entries:
+                row = [interpro_id] + ["" for _ in name_dict]
+                for entry in entries:
+                    if entry[0] in name_dict:
+                        row[list(name_dict.keys()).index(entry[0])] = entry[1]
+            writer.writerow(row)
 
 #==================================================================================================================
 # 											main
@@ -42,36 +50,29 @@ def main():
     usage = "python Interpro_parse.py -i <input_file> -o <output_folder> \n"
     parser = OptionParser(usage)
     parser.add_option("-i", "--input_file", dest="input_file", help="path for the Interpro dataset")
-    parser.add_option("-o", "--output_folder", dest="output_folder", help="path to the folder where the data will be saved")
+    parser.add_option("-o", "--output_file", dest="output_file", help="path for the file parsed")
 
-    (option, args) = parser.parse_args()
-    input_file = option.input_file
-    output_file = option.output_file
+    (options, args) = parser.parse_args()
+    input_file = options.input_file
+    output_file = options.output_file
 
     try:
         xml = open(input_file, "r", encoding='utf-8')
     except:
         print(' error with -i : correspond to the path of the dataset ')
 
-    all_db = {} # {db = [[id,name], ...], ...} que le shortname est utile !
-
-    map_interpro = {} 
-
-    ec_number = {} # { Interpro_id = [ec_1, ec_2, ...], ...}
-                   # si aucun ec, l'interpro_id n'est pas dans le dic
-
-    context = ET.iterparse(xml, events=('start', 'end'))
+    all_db = {}
+    map_interpro = {}
+    ec_number = {}
     interpro_id = None 
     shortname = None
     name = None
-    ec_number = {}
-    all_db = {}
-    map_interpro = {}
-
     res_tag = None
     bool_tag = False
+    
+    context = ET.iterparse(xml, events=('start', 'end'))
 
-    for event, elem in context:
+    for event, elem in tqdm(context, desc="Processing XML"):
         
         if event == 'start' and elem.tag == 'interpro':
             interpro_id = elem.attrib.get('id')
@@ -93,7 +94,7 @@ def main():
                 if interpro_id not in map_interpro:
                     map_interpro[interpro_id] = []
                 if db not in [entry[0] for entry in all_db.get('member_list', [])]:
-                    map_interpro[interpro_id].append(f"{dbkey} ")
+                    map_interpro[interpro_id].append([db,dbkey])
 
         elif event == 'start' and elem.tag == 'db_xref' and elem.attrib.get('db') == 'EC':
             if interpro_id not in ec_number:
@@ -109,9 +110,9 @@ def main():
             bool_tag = False
 
     writter_db_id_name(all_db, output_file)
+    writter_map_interpro(map_interpro, all_db, output_file)
     writter_ec(ec_number, output_file)
 
-    
 #==================================================================================================================
 if __name__ == "__main__":
 	main()
