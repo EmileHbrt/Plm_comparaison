bc5d93c8d2688620c2f76c499b9f70337c7bf199
Emile Hembert <emile.hembert@gmail.com>
2025-03-26 16:13:43 +0100

    Retour au chemin relatif

--- code/Uni_parse.py
+++ code/Uni_parse.py
@@ -27,8 +27,7 @@ def main():
                         break
                     else:
                         name += elm
-                file_name = os.path.abspath(output_dir)
-                file_name = os.path.join(file_name, f"{name}.dat")
+                file_name = os.path.join(output_dir, f"{name}.dat")
                 txt += ligne
 
             elif arg_start in ['ID', 'DE', 'OC', 'DR','SQ','  ']:
